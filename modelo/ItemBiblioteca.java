807ccb5929e031171b3d3bdb5beacfb4911ad959
GilcimarMoraes <myrraws@yahoo.com.br>
2025-11-25 01:07:47 -0300

    implementacao primeiro metodo

--- modelo/ItemBiblioteca.java
+++ modelo/ItemBiblioteca.java
@@ -1,5 +1,33 @@
 package modelo;
 
-public abstract class ItemBiblioteca {
+public abstract class ItemBiblioteca implements Emprestavel {
 
+    protected String titulo;
+    protected Autor autor;
+    protected Categoria categoria;
+    protected int anoPublicacao;
+    protected boolean disponivel;
+    protected String usuarioEmprestimo;
+
+    public ItemBiblioteca(String titulo, Autor autor, Categoria categoria, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = true; // por padrão, o item está disponível
+        this.usuarioEmprestimo = null; // nenhum usuário inicialmente
+    }
+    
+    // Implementação básicos dos métodos da interface Emprestavel
+    @Override
+    public void emprestar(String usuario) {
+        if( disponivel ) {
+            this.disponivel = false;
+            this.usuarioEmprestimo = usuario;
+            System.out.println( titulo + " emprestado para " + usuario + "." );
+        } 
+        else {
+            System.out.println( "Item indisponível para empréstimo." );
+        }
+    }
 }
